509c1246269f77edcb0a9dd95ec791df71a7279a
Sergey98Am <serg98barca@gmail.com>
2021-11-24 17:53:34 +0400

    initial commit

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -815,4 +815,4 @@ $databases['default']['default'] = array (
 $settings['trusted_host_patterns'] = [
   '^drupal-course.loc$',
 ];
-$settings['config_sync_directory'] = 'sites/default/files/config_SiHO7F8qtXQqaY9yUxBKBWCIlDLYaRTaGfBvluOLuDR4iRjIcxDSzil4s4M4eVGQ18c5e5C6xQ/sync';
+$settings['config_sync_directory'] = 'config/sync';
